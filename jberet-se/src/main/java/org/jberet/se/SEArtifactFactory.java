fe2f860d8adcaf59e215396f9222f396320fb6ef
Cheng Fang <cfang@redhat.com>
2015-04-21 22:03:58 -0400

    JBERET-159 Use BeanManager#resolve instead of iteration in SEArtifactFactory

--- jberet-se/src/main/java/org/jberet/se/SEArtifactFactory.java
+++ jberet-se/src/main/java/org/jberet/se/SEArtifactFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2013 Red Hat, Inc. and/or its affiliates.
+ * Copyright (c) 2012-2015 Red Hat, Inc. and/or its affiliates.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,8 +12,6 @@
 
 package org.jberet.se;
 
-import java.util.Iterator;
-import java.util.Set;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
 
@@ -28,27 +26,23 @@ public final class SEArtifactFactory extends AbstractArtifactFactory {
     private final BeanManager beanManager;
 
     public SEArtifactFactory() {
-        WeldContainer weldContainer = new Weld().initialize();
+        final WeldContainer weldContainer = new Weld().initialize();
         beanManager = weldContainer.getBeanManager();
     }
 
     @Override
     public Class<?> getArtifactClass(final String ref, final ClassLoader classLoader) {
-        Bean<?> bean = getBean(ref);
+        final Bean<?> bean = getBean(ref);
         return bean == null ? null : bean.getBeanClass();
     }
 
     @Override
-    public Object create(final String ref, Class<?> cls, final ClassLoader classLoader) throws Exception {
-        Bean<?> bean = getBean(ref);
+    public Object create(final String ref, final Class<?> cls, final ClassLoader classLoader) throws Exception {
+        final Bean<?> bean = getBean(ref);
         return bean == null ? null : beanManager.getReference(bean, bean.getBeanClass(), beanManager.createCreationalContext(bean));
     }
 
     private Bean<?> getBean(final String ref) {
-        Set<Bean<?>> beans = beanManager.getBeans(ref);
-        for (Iterator<Bean<?>> it = beans.iterator(); it.hasNext(); ) {
-            return it.next();
-        }
-        return null;
+        return beanManager.resolve(beanManager.getBeans(ref));
     }
 }
