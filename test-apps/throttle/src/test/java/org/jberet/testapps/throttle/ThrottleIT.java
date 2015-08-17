e2f772c0636c515d0e180e1a5f41f428adde8287
Cheng Fang <cfang@redhat.com>
2015-08-17 15:37:36 -0400

    update test org.jberet.testapps.throttle.ThrottleIT to increase the number of job starts, and add an option for concurrent job start by the client.

--- test-apps/throttle/src/test/java/org/jberet/testapps/throttle/ThrottleIT.java
+++ test-apps/throttle/src/test/java/org/jberet/testapps/throttle/ThrottleIT.java
@@ -14,9 +14,8 @@ package org.jberet.testapps.throttle;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.concurrent.CopyOnWriteArrayList;
 import javax.batch.runtime.BatchStatus;
-import javax.batch.runtime.JobExecution;
 
 import org.jberet.runtime.JobExecutionImpl;
 import org.jberet.testapps.common.AbstractIT;
@@ -31,30 +30,67 @@ import org.junit.Test;
  * <p/>
  * The purpose of the tests is to verify that the batch runtime should be able to detect the potential deadlock risk
  * when starting a new job executions, and queue it up for later execution when threads are available, to avoid deadlock.
- *
+ * <p/>
  * The batch runtime is configured to have a fixed thread pool of size 10, in src/main/resources/jberet.properties
  *
  * @see <a href="https://issues.jboss.org/browse/JBERET-180">JBERET-180</a>
  */
-@Ignore
 public class ThrottleIT extends AbstractIT {
     static final String jobName = "throttle";
 
     /**
-     * Starts the test job 50 times, one after another.  The test has a chunk-type step with 10 partitioins.
+     * Starts the test job 300 times, one after another or concurrently.
+     * The test has a chunk-type step with 10 partitioins.
      * Verifies the job submission is properly throttled and the these jobs should complete successfully without deadlock.
      *
      * @throws Exception
      */
     @Test
-    public void start50() throws Exception {
-        runTest(50);
+    public void start300() throws Exception {
+        runTest(300, true);
     }
 
-    private void runTest(final int count) throws Exception {
-        final List<Long> jobExecutionIds = new ArrayList<Long>();
-        for (int i = 0; i < count; ++i) {
-            jobExecutionIds.add(jobOperator.start(jobName, params));
+    @Ignore
+    @Test
+    public void start1000() throws Exception {
+        runTest(1000, true);
+    }
+
+    @Ignore
+    @Test
+    public void start2100() throws Exception {
+        runTest(2100, true);
+    }
+
+    /**
+     *
+     * @param count number of times to start the test job
+     * @param concurrent a flag whether to start the test job serially or concurently
+     *
+     * @throws Exception
+     */
+    private void runTest(final int count, final boolean concurrent) throws Exception {
+        final List<Long> jobExecutionIds = new CopyOnWriteArrayList<Long>();
+
+        if(concurrent) {
+            final List<Thread> threads = new ArrayList<Thread>();
+            for (int i = 0; i < count; i++) {
+                final Thread t = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        jobExecutionIds.add(jobOperator.start(jobName, params));
+                    }
+                });
+                threads.add(t);
+                t.start();
+            }
+            for (final Thread t : threads) {
+                t.join();
+            }
+        } else {
+            for (int i = 0; i < count; ++i) {
+                jobExecutionIds.add(jobOperator.start(jobName, params));
+            }
         }
 
         for (final Long id : jobExecutionIds) {
