0b86780bc4a2c745618ee7fa63595cb093d8c607
Cheng Fang <cfang@redhat.com>
2016-06-21 22:22:36 -0400

    JBERET-244 Implement Apache Camel ItemWriter

--- jberet-camel/src/main/java/org/jberet/camel/CamelItemWriter.java
+++ jberet-camel/src/main/java/org/jberet/camel/CamelItemWriter.java
@@ -14,8 +14,15 @@ package org.jberet.camel;
 
 import java.io.Serializable;
 import java.util.List;
+import javax.batch.api.BatchProperty;
 import javax.batch.api.chunk.ItemWriter;
+import javax.batch.operations.BatchRuntimeException;
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
 
 /**
  * Implementation of {@code javax.batch.api.chunk.ItemWriter} that writes batch data
@@ -24,10 +31,26 @@ import javax.inject.Named;
  * @since 1.3.0
  */
 @Named
+@Dependent
 public class CamelItemWriter extends CamelArtifactBase implements ItemWriter {
+
+    @Inject
+    @BatchProperty
+    protected String endpoint;
+
+    @Inject
+    protected CamelContext camelContext;
+
+    protected ProducerTemplate producerTemplate;
+
     @Override
     public void open(final Serializable checkpoint) throws Exception {
-
+        if (camelContext == null) {
+            throw new BatchRuntimeException("CamelContext not available in " + this.getClass().getName());
+        }
+        if (producerTemplate == null) {
+            producerTemplate = camelContext.createProducerTemplate();
+        }
     }
 
     @Override
@@ -37,7 +60,9 @@ public class CamelItemWriter extends CamelArtifactBase implements ItemWriter {
 
     @Override
     public void writeItems(final List<Object> items) throws Exception {
-
+        for (final Object e : items) {
+            producerTemplate.sendBody(endpoint, e);
+        }
     }
 
     @Override
