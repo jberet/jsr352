828198fe52f903656be8fbdb57e3d562c5b45cbc
Cheng Fang <cfang@redhat.com>
2017-11-03 15:47:33 -0400

    add more vertx-based REST API for batch processing operations.

--- jberet-vertx/jberet-vertx-rest/src/main/java/org/jberet/vertx/rest/JBeretRouterConfig.java
+++ jberet-vertx/jberet-vertx-rest/src/main/java/org/jberet/vertx/rest/JBeretRouterConfig.java
@@ -24,6 +24,7 @@ import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 import org.jberet.rest.entity.JobEntity;
 import org.jberet.rest.entity.JobExecutionEntity;
+import org.jberet.rest.entity.StepExecutionEntity;
 import org.jberet.rest.service.JobService;
 
 /**
@@ -41,8 +42,12 @@ public class JBeretRouterConfig {
         router.post("/jobs/:jobXmlName/start").handler(JBeretRouterConfig::startJob);
 
         router.get("/jobexecutions/:jobExecutionId").handler(JBeretRouterConfig::getJobExecution);
+        router.get("/jobexecutions/:jobExecutionId/stepexecutions").handler(JBeretRouterConfig::getStepExecutions);
+        router.get("/jobexecutions/:jobExecutionId/stepexecutions/:stepExecutionId").handler(JBeretRouterConfig::getStepExecution);
+
         router.post("/jobexecutions/:jobExecutionId/abandon").handler(JBeretRouterConfig::abandonJobExecution);
         router.post("/jobexecutions/:jobExecutionId/stop").handler(JBeretRouterConfig::stopJobExecution);
+        router.post("/jobexecutions/:jobExecutionId/restart").handler(JBeretRouterConfig::restartJobExecution);
     }
 
     public static void getDefault(final RoutingContext routingContext) {
@@ -53,7 +58,7 @@ public class JBeretRouterConfig {
         final JobEntity[] jobEntities = JobService.getInstance().getJobs();
         final JsonArray jsonArray = new JsonArray();
         for (JobEntity jobEntity : jobEntities) {
-            jsonArray.add(jobEntity);
+            jsonArray.add(JsonObject.mapFrom(jobEntity));
         }
 
         routingContext.response().putHeader("content-type", "application/json")
@@ -62,15 +67,7 @@ public class JBeretRouterConfig {
 
     public static void startJob(final RoutingContext routingContext) {
         final String jobXmlName = routingContext.pathParam("jobXmlName");
-        final MultiMap params = routingContext.request().params();
-        final List<Map.Entry<String, String>> entries = params.entries();
-        final Properties jobParams = new Properties();
-        for (Map.Entry<String, String> entry : entries) {
-            final String key = entry.getKey();
-            final String value = entry.getValue();
-            jobParams.setProperty(key, value);
-        }
-
+        final Properties jobParams = getJobParameters(routingContext);
         final JobExecutionEntity jobExecutionEntity = JobService.getInstance().start(jobXmlName, jobParams);
         final JsonObject jsonObject = JsonObject.mapFrom(jobExecutionEntity);
         routingContext.response().putHeader("content-type", "application/json")
@@ -88,15 +85,75 @@ public class JBeretRouterConfig {
                 .end(jsonObject.encodePrettily());
     }
 
+    public static void getStepExecutions(final RoutingContext routingContext) {
+        final String jobExecutionIdString = routingContext.pathParam("jobExecutionId");
+        final long jobExecutionId = Long.parseLong(jobExecutionIdString);
+        final StepExecutionEntity[] stepExecutions = JobService.getInstance().getStepExecutions(jobExecutionId);
+
+        final JsonArray jsonArray = new JsonArray();
+        for (StepExecutionEntity e : stepExecutions) {
+            jsonArray.add(JsonObject.mapFrom(e));
+        }
+
+        routingContext.response().putHeader("content-type", "application/json")
+                .end(jsonArray.encodePrettily());
+    }
+
+    public static void getStepExecution(final RoutingContext routingContext) {
+        final String jobExecutionIdString = routingContext.pathParam("jobExecutionId");
+        final long jobExecutionId = Long.parseLong(jobExecutionIdString);
+        final String stepExecutionIdString = routingContext.pathParam("stepExecutionId");
+        final long stepExecutionId = Long.parseLong(stepExecutionIdString);
+
+        StepExecutionEntity stepExecutionFound = null;
+        final StepExecutionEntity[] stepExecutions = JobService.getInstance().getStepExecutions(jobExecutionId);
+        for (StepExecutionEntity e : stepExecutions) {
+            if (e.getStepExecutionId() == stepExecutionId) {
+                stepExecutionFound = e;
+            }
+        }
+
+        final JsonObject jsonObject = JsonObject.mapFrom(stepExecutionFound);
+        routingContext.response().putHeader("content-type", "application/json")
+                .end(jsonObject.encodePrettily());
+    }
+
     public static void abandonJobExecution(final RoutingContext routingContext) {
         final String jobExecutionIdString = routingContext.pathParam("jobExecutionId");
         final long jobExecutionId = Long.parseLong(jobExecutionIdString);
-        final JobExecutionEntity jobExecutionEntity = JobService.getInstance().getJobExecution(jobExecutionId);
-
-
+        JobService.getInstance().abandon(jobExecutionId);
+        routingContext.response().end();
     }
 
     public static void stopJobExecution(final RoutingContext routingContext) {
-
+        final String jobExecutionIdString = routingContext.pathParam("jobExecutionId");
+        final long jobExecutionId = Long.parseLong(jobExecutionIdString);
+        JobService.getInstance().stop(jobExecutionId);
+        routingContext.response().end();
     }
+
+    public static void restartJobExecution(final RoutingContext routingContext) {
+        final String jobExecutionIdString = routingContext.pathParam("jobExecutionId");
+        final long jobExecutionId = Long.parseLong(jobExecutionIdString);
+        final Properties jobParams = getJobParameters(routingContext);
+        final JobExecutionEntity jobExecutionEntity = JobService.getInstance().restart(jobExecutionId, jobParams);
+
+        final JsonObject jsonObject = JsonObject.mapFrom(jobExecutionEntity);
+        routingContext.response().putHeader("content-type", "application/json")
+                .end(jsonObject.encodePrettily());
+    }
+
+
+    private static Properties getJobParameters(final RoutingContext routingContext) {
+        final MultiMap params = routingContext.request().params();
+        final List<Map.Entry<String, String>> entries = params.entries();
+        final Properties jobParams = new Properties();
+        for (Map.Entry<String, String> entry : entries) {
+            final String key = entry.getKey();
+            final String value = entry.getValue();
+            jobParams.setProperty(key, value);
+        }
+        return jobParams;
+    }
+
 }
