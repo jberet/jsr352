97a51ebcc6d67be68bc039c696b3e7d34cb7b32d
Cheng Fang <cfang@redhat.com>
2022-05-12 12:08:15 -0400

    JBERET-551 Support injection of JobOperator (#223)

--- jberet-core/src/main/java/org/jberet/creation/BatchCDIExtension.java
+++ jberet-core/src/main/java/org/jberet/creation/BatchCDIExtension.java
@@ -10,13 +10,19 @@
 
 package org.jberet.creation;
 
+import jakarta.batch.operations.JobOperator;
 import jakarta.enterprise.event.Observes;
 import jakarta.enterprise.inject.spi.AfterBeanDiscovery;
 import jakarta.enterprise.inject.spi.AnnotatedType;
+import jakarta.enterprise.inject.spi.Bean;
 import jakarta.enterprise.inject.spi.BeanManager;
 import jakarta.enterprise.inject.spi.BeforeBeanDiscovery;
+import jakarta.enterprise.inject.spi.ProcessBean;
+import org.jberet._private.BatchLogger;
 
 public class BatchCDIExtension implements jakarta.enterprise.inject.spi.Extension {
+    private Boolean applicationDefinedJobOperatorProducer;
+
     public void beforeBeanDiscovery(@Observes final BeforeBeanDiscovery beforeBeanDiscovery, final BeanManager beanManager) {
         final AnnotatedType<BatchBeanProducer> batchProducerAnnotatedType = beanManager.createAnnotatedType(BatchBeanProducer.class);
         beforeBeanDiscovery.addAnnotatedType(batchProducerAnnotatedType, BatchBeanProducer.class.getName());
@@ -26,5 +32,27 @@ public class BatchCDIExtension implements jakarta.enterprise.inject.spi.Extensio
         afterBeanDiscovery.addContext(JobScopedContextImpl.INSTANCE);
         afterBeanDiscovery.addContext(StepScopedContextImpl.INSTANCE);
         afterBeanDiscovery.addContext(PartitionScopedContextImpl.INSTANCE);
+    }
+
+    public <A> void processBean(final @Observes ProcessBean<A> processBeanEvent) {
+        if (applicationDefinedJobOperatorProducer == null) {
+            final Bean<A> bean = processBeanEvent.getBean();
+            if (bean.getTypes().contains(JobOperator.class)) {
+                if (bean.getBeanClass() == JobOperatorProducer.class) {
+                    applicationDefinedJobOperatorProducer = Boolean.FALSE;
+                } else {
+                    applicationDefinedJobOperatorProducer = Boolean.TRUE;
+                    if (BatchLogger.LOGGER.isDebugEnabled()) {
+                        BatchLogger.LOGGER.debugf("Use application-defined JobOperator producer: %s", bean.getBeanClass());
+                    }
+                }
+            }
+        }
+    }
+
+    public void afterBeanDiscovery(final @Observes AfterBeanDiscovery abd) {
+        if (applicationDefinedJobOperatorProducer == null) {
+            abd.addBean(new JobOperatorProducer());
+        }
     }
 }
