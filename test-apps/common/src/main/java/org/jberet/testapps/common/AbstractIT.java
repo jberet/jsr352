9de8feadff0457adc1f14b5aac5d2b83c5683de8
阿男 <l.weinan@gmail.com>
2023-10-13 02:00:39 +0800

    Fix the random test failure of purgeInMemoryRepository #387

--- test-apps/common/src/main/java/org/jberet/testapps/common/AbstractIT.java
+++ test-apps/common/src/main/java/org/jberet/testapps/common/AbstractIT.java
@@ -14,6 +14,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
 import jakarta.batch.operations.NoSuchJobExecutionException;
 import jakarta.batch.runtime.JobExecution;
 import jakarta.batch.runtime.JobInstance;
@@ -97,13 +99,21 @@ abstract public class AbstractIT {
         jobExecution = (JobExecutionImpl) jobOperator.getJobExecution(jobExecutionId);
     }
 
+    private ReentrantLock termLock = new ReentrantLock();
+
     protected void awaitTermination(final JobExecutionImpl... exes) throws InterruptedException {
-        final JobExecutionImpl exe = exes.length == 0 ? jobExecution : exes[0];
-        exe.awaitTermination(getJobTimeoutSeconds(), TimeUnit.SECONDS);
-        stepExecutions = jobOperator.getStepExecutions(exe.getExecutionId());
-        if (!stepExecutions.isEmpty()) {
-            stepExecution0 = (StepExecutionImpl) stepExecutions.get(0);
+        try {
+            termLock.lock();
+            final JobExecutionImpl exe = exes.length == 0 ? jobExecution : exes[0];
+            exe.awaitTermination(getJobTimeoutSeconds(), TimeUnit.SECONDS);
+            stepExecutions = jobOperator.getStepExecutions(exe.getExecutionId());
+            if (!stepExecutions.isEmpty()) {
+                stepExecution0 = (StepExecutionImpl) stepExecutions.get(0);
+            }
+        } finally {
+            termLock.unlock();
         }
+
     }
 
     protected void startJobAndWait(final String jobXml) throws Exception {
