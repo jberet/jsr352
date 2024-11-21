e0a8c79f3b149c039443a537729b140454a375a6
阿男 <l.weinan@gmail.com>
2024-11-21 23:17:34 +0800

    refactor

--- jberet-se/src/test/java/org/jberet/se/test/SleepBatchletTest.java
+++ jberet-se/src/test/java/org/jberet/se/test/SleepBatchletTest.java
@@ -18,7 +18,10 @@ import jakarta.batch.runtime.BatchRuntime;
 import jakarta.batch.runtime.BatchStatus;
 import jakarta.batch.runtime.StepExecution;
 
+import org.jberet.operations.ForceStopJobOperatorImpl;
 import org.jberet.runtime.JobExecutionImpl;
+import org.jberet.spi.ForceStopJobOperatorContextSelector;
+import org.jberet.spi.JobOperatorContext;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -67,6 +70,33 @@ public class SleepBatchletTest {
         Assertions.assertEquals(BatchStatus.STOPPED, jobExecution.getBatchStatus());
         Assertions.assertEquals(BatchStatus.STOPPED.name(), jobExecution.getExitStatus());
     }
+
+
+    @Test
+    public void sleepForceStop() throws Exception {
+
+        JobOperatorContext.setJobOperatorContextSelector(new ForceStopJobOperatorContextSelector());
+        ForceStopJobOperatorImpl operator = (ForceStopJobOperatorImpl) JobOperatorContext.getJobOperatorContext().getJobOperator();
+
+        final int sleepMinutes = 6;
+        final Properties params = new Properties();
+        params.setProperty("sleep.minutes", String.valueOf(sleepMinutes));
+        final long jobExecutionId = operator.start(jobName, params);
+        final JobExecutionImpl jobExecution = (JobExecutionImpl) operator.getJobExecution(jobExecutionId);
+
+        operator.forceStop(jobExecutionId);
+        // don't await.
+//        jobExecution.awaitTermination(1, TimeUnit.MINUTES);
+
+        System.out.printf("jobExecution id=%s, batchStatus=%s, exitStatus=%s, jobParameters=%s, restartPosition=%s, " +
+                        "createTime=%s, startTime=%s, lastUpdateTime=%s, endTime=%s%n",
+                jobExecutionId, jobExecution.getBatchStatus(), jobExecution.getExitStatus(), jobExecution.getJobParameters(),
+                jobExecution.getRestartPosition(), jobExecution.getCreateTime(), jobExecution.getStartTime(),
+                jobExecution.getLastUpdatedTime(), jobExecution.getLastUpdatedTime(), jobExecution.getEndTime());
+        Assertions.assertEquals(BatchStatus.STOPPED, jobExecution.getBatchStatus());
+        Assertions.assertEquals(BatchStatus.STOPPED.name(), jobExecution.getExitStatus());
+    }
+
 
     /**
      * Verifies that exception from JobListener.beforeJob() method will cause the job to fail.
